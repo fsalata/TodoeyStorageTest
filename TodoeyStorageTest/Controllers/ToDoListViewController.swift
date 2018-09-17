70d7b36e95cc3f4190da015624cfb753c9eca443
Fábio Salata <fsalata@gmail.com>
2018-09-17 13:35:26 -0300

    UserDefaults storage

--- TodoeyStorageTest/Controllers/ToDoListViewController.swift
+++ TodoeyStorageTest/Controllers/ToDoListViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    var itemArray = ["Test", "Test 1", "Test 2" ]
+    var itemArray = []
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
     
     //MARK - TableView Datasource Methods
@@ -54,6 +59,8 @@ class ToDoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add item", style: .default) { (action) in
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.tableView.reloadData()
         }
---
+++ TodoeyStorageTest/Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  TodoeyStorageTest
+//
+//  Created by Fabio Salata on 17/09/18.
+//  Copyright © 2018 Fabio Salata. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}

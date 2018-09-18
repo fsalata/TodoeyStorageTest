6cb5da108ff40c4a60ac2df5424e7544dba29713
Fábio Salata <fsalata@gmail.com>
2018-09-18 15:20:25 -0300

    To do list save, edit and load from Core Data

--- TodoeyStorageTest/Controllers/ToDoListViewController.swift
+++ TodoeyStorageTest/Controllers/ToDoListViewController.swift
@@ -90,15 +90,35 @@ class ToDoListViewController: UITableViewController {
         tableView.reloadData()
     }
     
-    fileprivate func loadItems() {
-        let request: NSFetchRequest<Item> = Item.fetchRequest()
-        
+    fileprivate func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
         do {
             itemArray = try context.fetch(request)
         } catch {
             print("error fetching request")
         }
+        
+        tableView.reloadData()
     }
-    
 }
 
+extension ToDoListViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [ NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+}
